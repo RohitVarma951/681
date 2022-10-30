03d133112dac862f09b95459695495c1fc248c73
RohitVarma951 <100976048+RohitVarma951@users.noreply.github.com>
2022-10-30 00:18:16 -0400

    Delete HW8 directory

--- HW8/src/edu/umb/cs681/factorizer/RunnablePrimeFactorizer.java
+++
@@ -1,7 +0,0 @@
-package edu.umb.cs681.factorizer;
-
-public abstract class RunnablePrimeFactorizer extends PrimeFactorizer implements Runnable {
-    public RunnablePrimeFactorizer(long number){
-        super(number);
-    }
-}
