60d865221ab0b39881f9439af1cfadf951553f36
RohitVarma951 <100976048+RohitVarma951@users.noreply.github.com>
2022-10-30 00:18:08 -0400

    Delete HW7 directory

--- HW7/src/edu/umb/cs681/primes/RunnablePrimeGenerator.java
+++
@@ -1,7 +0,0 @@
-package edu.umb.cs681.primes;
-
-public abstract class RunnablePrimeGenerator extends PrimeGenerator implements Runnable{
-    public RunnablePrimeGenerator(long from,long to){
-        super(from,to);
-    }
-}
