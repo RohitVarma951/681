3c5cdc862b36af4856db4c9c0cd7efef585a91ac
RohitVarma951 <100976048+RohitVarma951@users.noreply.github.com>
2022-11-15 13:27:22 -0500

    Add files via upload

---
+++ HW9/src/edu/umb/cs681/singleton/ConcurrentSingleton.java
@@ -0,0 +1,25 @@
+package edu.umb.cs681.singleton;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ConcurrentSingleton {
+    private ConcurrentSingleton(){}
+    private static ConcurrentSingleton instance = null;
+    private static final ReentrantLock lock = new ReentrantLock();
+
+    public static ConcurrentSingleton getInstance(){
+        lock.lock();
+        try{
+            if(instance==null)
+                instance = new ConcurrentSingleton();
+            return instance;
+        }finally{
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args){
+        for(int i=0;i<5;i++)
+            new Thread(()->{System.out.println(ConcurrentSingleton.getInstance());}).start();
+    }
+}
